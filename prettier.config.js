eb66bba2b288e3266468a64a4fe7dc733a050274
nothing <ddwi2504@gmail.com>
2022-08-07 18:46:23 +0800

    halo

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 // https://prettier.io/docs/en/options.html
 /** @type {import('prettier').RequiredOptions} */
 module.exports = {
-  trailingComma: 'es6',
+  trailingComma: 'es5',
   bracketSpacing: true,
   tabWidth: 2,
   semi: true,
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,3 +1,5 @@
+import { Link, routes } from "@redwoodjs/router"
+
 const HomePage = () => {
   return (
     <>
@@ -7,6 +9,12 @@ const HomePage = () => {
         this page is under construction so type on the url like
         &quot;/test&quot;
       </p>
+
+      <Link to={routes.card()}>card</Link>
+      <br/>
+      <Link to={routes.loading()}>loading</Link>
+      <br/>
+      <Link to={routes.login()}>login</Link>
     </>
   )
 }
